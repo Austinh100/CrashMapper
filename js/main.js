3ec3996ca4ebea533ab6f620a48a132291070e30
Austinh100 <austinh100@yahoo.com>
2014-02-09 06:02:48 -0600

    added car placment on the map, car rotation and lines for rotation, added save button (not working fully right now)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
+var draw;
+var mouseLine;
+var line = false;
+var activeCar;
+var mouseX = 0;
+var mouseY = 0;
+var curLine;
+
 $( document ).ready(function() {
     console.log( "ready!" );
     console.log($(this).height());
+    
+    draw = SVG('backImg')
+    var image;
+    
+    console.log($(this).innerHeight());
+    var parWidth = $("#mapContainer").css("width");
+    parWidth = parWidth.substring(0, parWidth.length - 2);
+    image = draw.image(buildGoogleURL($(this).height(), parWidth, "20", "king%20and%2031st,chicago,il")).loaded(function(loader) {
+          this.size(loader.width, loader.height)
+    });
+    
+    
+    
+    
+  /*  
     updateMaxHeight();
 
     var rtime = new Date(1, 1, 2000, 12,00,00);
@@ -23,36 +46,123 @@ $( document ).ready(function() {
         }               
     }
     
-    function updateMaxHeight () {
-        console.log($(this).height());
-        var parWidth = $("#backImg").parent().css("width");
+    function updateMaxHeight() {
+        console.log($(this).innerHeight());
+        var parWidth = $("#mapContainer").css("width");
         parWidth = parWidth.substring(0, parWidth.length - 2);
-        $("#backImg").attr("src", buildGoogleURL($(this).height(), parWidth, "20", "king%20and%2031st,chicago,il"));
-    }
+        image.replace(draw.image(buildGoogleURL($(this).height(), parWidth, "20", "king%20and%2031st,chicago,il")).loaded(function(loader) {
+              this.size(loader.width, loader.height)
+        }));
+    }*/
     
     function buildGoogleURL(height, width, zoom, loc) {
         return "http://maps.googleapis.com/maps/api/staticmap?center="+loc+"&zoom="+zoom+"&size=" +height+"x"+width+"&sensor=false&style=feature:transit|element:all|visibility:off&style=feature:poi|element:all|visibility:off&style=feature:landscape|element:all|visibility:off&style=feature:administrative|element:all|visibility:off&scale=3&maptype=hybrid";
     }
     
+    $("#backImg").mousemove(function(e){
+        var wrapper = $(this).parent();
+        var parentOffset = wrapper.offset(); 
+        var relX = e.pageX - parentOffset.left + wrapper.scrollLeft();
+        var relY = e.pageY - parentOffset.top + wrapper.scrollTop();
+        
+        mouseX = relX;
+        mouseY = relY;
+        
+        if(line) {
+            mouseLine.plot(mouseLine.x1, mouseLine.y1, mouseX, mouseY);
+        }
+    });
+    
+    $("#backImg").click(function(e){
+        if(line) {
+            mapCars[activeCar].image.rotate(calcAngle(curLine.x1, curLine.y1, mouseX, mouseY));
+            console.log(calcAngle(curLine.x1, curLine.y1, mouseX, mouseY));
+            line = false;
+            mouseLine.remove();
+        } else {
+            placeCar(carToPlace,mouseX,mouseY);
+        }
+    });
+    
+    $("#saveImage").click(function(e) {
+        console.log($('svg').attr('id'));
+       canvg('canvas', $('svg').get(0));
+
+        // the canvas calls to output a png
+        var canvas = document.getElementById("canvas");
+        var img = canvas.toDataURL("image/png");
+        
+        var parWidth = $("#mapContainer").css("width");
+        parWidth = parWidth.substring(0, parWidth.length - 2);
+        $.slimbox(img);
+        
+    });
 });
 
 var carToPlace = 0;
-
-function mapClicked() {
-    
-    
-    placeCar(carToPlace);
-}
+var mapCars = [];
 
 function carClicked(num) {
     $("#cars"+num).addClass("grayscale");
     carToPlace = num;
 }
 
-function placeCar(num) {
+function placeCar(num, x, y) {
     if(carToPlace != 0) {
-        
+        newMapCar(carToPlace, x, y);
         $("#cars"+num).removeClass("grayscale");
     }
     carToPlace = 0;
+}
+
+function mapCarPlaced(num) {
+    
+}
+
+function newMapCar(num, x, y) {
+    mapCars.push({ 'image': draw.image('img/car'+num+'.png'), 'type': num });
+    
+    mapCars[mapCars.length-1].image.x(x-30).y(y-15);
+    mouseLine = draw.line(x, y, mouseX, mouseY).stroke({ width: 3 })
+    curLine = {
+        x1: x,
+        y1: y
+    };
+    activeCar = mapCars.length-1;
+    line = true;
+    return 'carms'+num;
+}
+
+/*
+function newMapCar(num, x, y) {
+    $("#mapContainer").append('<a id="carid'+mapCars.length+'" href="#" id="mapCarCopy" onclick="mapCarClicked('+num+')"><div class="carm'+num+'" style="position:absolute;top:0;left:0;"></div></a>');
+    $("#carid"+mapCars.length+" div").css({
+        left: x-30,
+        top: y-15
+    });
+    mapCars.push({cartNum: carToPlace, rotation: 0});
+    return 'carms'+num;
+}*/
+
+function addArrow() {
+
+}
+
+function calcAngle(x1, y1, x2, y2) {
+    var p1 = {
+        x: x1,
+        y: y1
+    };
+     
+    var p2 = {
+        x: x2,
+        y: y2
+    };
+     
+    // angle in radians
+    var angleRadians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+     
+    // angle in degrees
+    var angleDeg = Math.atan2((p2.y - p1.y)*-1, p2.x - p1.x) * 180 / Math.PI;
+    return -angleDeg;
 }
